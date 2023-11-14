4e14ec59279c4ba4cdaaaee45fd76a01a2085d12
BobojonovSobir0576 <dweb0987654321@gmail.com>
2023-11-14 14:50:01 +0500

    git push

--- plants/admin.py
+++ plants/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from plants.models import (
+    PlantCategories,
+    Plants,
+    PlantImages,
+    CarePlanting,
+    CareTopics,
+    CareTopicHistory,
+    LocationPlantMarket,
+    CarePlantingTree
+)
+
+
+admin.site.register(PlantCategories)
+admin.site.register(Plants)
+admin.site.register(PlantImages)
+admin.site.register(CarePlanting)
+admin.site.register(CareTopics)
+admin.site.register(CareTopicHistory)
+admin.site.register(LocationPlantMarket)
+admin.site.register(CarePlantingTree)

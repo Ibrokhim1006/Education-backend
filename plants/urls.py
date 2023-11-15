59acd8004b374301ccbc72e98ca5a13dbb5b1258
Ibrokhim1006 <Ibroxim.2001@mail.ru>
2023-11-15 08:57:10 +0300

    sdsd

--- plants/urls.py
+++ plants/urls.py
@@ -30,7 +30,7 @@ from plants.views.plant_app_views import (
     CarePlantingSearchView,
     PlantSearchView,
     CareTopicGetView,
-    RecentlyUploadedView
+    RecentlyUploadedView,
 )
 
 urlpatterns = [
@@ -42,36 +42,71 @@ urlpatterns = [
     path("dash/plants_views/", PlantsViews.as_view()),
     path("dash/plants_crud_views/<int:pk>/", PlantsCrudViews.as_view()),
     path("dash/plants_img_crud_views/<int:pk>/", PlantsImgCrudViews.as_view()),
-    path('dash/care_planting_views/', CarePlantingViews.as_view()),
+    path("dash/care_planting_views/", CarePlantingViews.as_view()),
+    path("dash/care_planting_crud_views/<int:pk>/", CarePlantingCrudViews.as_view()),
+    path("dash/care_topics_views/", CareTopicsViews.as_view()),
+    path("dash/care_topics_crud_views/<int:pk>/", CareTopicsCrudViews.as_view()),
+    path("dash/location_plant_market_views/", LocationPlantMarketViews.as_view()),
     path(
-        'dash/care_planting_crud_views/<int:pk>/',
-        CarePlantingCrudViews.as_view()),
-    path('dash/care_topics_views/', CareTopicsViews.as_view()),
-    path(
-        'dash/care_topics_crud_views/<int:pk>/',
-        CareTopicsCrudViews.as_view()),
-    path(
-        'dash/location_plant_market_views/',
-        LocationPlantMarketViews.as_view()),
-    path(
-        'dash/location_plant_market_crud_views/<int:pk>/',
-        LocationPlantMarketCrudViews.as_view()),
-    path('dash/care_topic_history_views/', CareTopicHistoryViews.as_view()),
+        "dash/location_plant_market_crud_views/<int:pk>/",
+        LocationPlantMarketCrudViews.as_view(),
+    ),
+    path("dash/care_topic_history_views/", CareTopicHistoryViews.as_view()),
     # plants app
-    path('app/plant-categories/', PlantCategoriesView.as_view(), name='plant-categories'),
-    path('app/plant-popular/', PlantPopularView.as_view(), name='plant-popular'),
-    path('app/plant-filter-categories/<int:id>/', PlantFilterCategoriesView.as_view(), name='plant-filter-categories'),
-    path('app/plant-profile/<int:id>/', PlantGetView.as_view(), name='plant-profile'),
-    path('app/plant-care/', CarePlantingView.as_view(), name='plant-care'),
-    path('app/plant-care/<int:id>/', CarePlantingGetView.as_view(), name='plant-care'),
-    path('app/plant-care-topic/<int:id>/', CareTopicsView.as_view(), name='plant-care-topic'),
-    path('app/plant-care-topic-get/', CareTopicGetView.as_view(), name='plant-care-topic-get'),
-    path('app/plant-care-topic-history/', CareTopicsHistoryView.as_view(), name='plant-care-topic-history'),
-    path('app/plant-location-market/', LocationMarketView.as_view(), name='plant-location-market'),
-    path('app/plant-care-trees/', CarePlantingTreeView.as_view(), name='plant-care-trees'),
-    path('app/plant-recently-viewed/', LastRecentlyViewedPlantView.as_view(), name='plant-recently-viewed'),
-    path('app/plant-recently-uploaded/', RecentlyUploadedView.as_view(), name='plant-recently-uploaded'),
-    path('app/plant-search/', PlantSearchView.as_view(), name='plant-search'),
-    path('app/plant-care_search/', CarePlantingSearchView.as_view(), name='plant-care_search'),
-    path('app/plant-care_tree_search/', CarePlantingTreeSearchView.as_view(), name='plant-care_tree_search'),
+    path(
+        "app/plant-categories/", PlantCategoriesView.as_view(), name="plant-categories"
+    ),
+    path("app/plant-popular/", PlantPopularView.as_view(), name="plant-popular"),
+    path(
+        "app/plant-filter-categories/<int:id>/",
+        PlantFilterCategoriesView.as_view(),
+        name="plant-filter-categories",
+    ),
+    path("app/plant-profile/<int:id>/", PlantGetView.as_view(), name="plant-profile"),
+    path("app/plant-care/", CarePlantingView.as_view(), name="plant-care"),
+    path("app/plant-care/<int:id>/", CarePlantingGetView.as_view(), name="plant-care"),
+    path(
+        "app/plant-care-topic/<int:id>/",
+        CareTopicsView.as_view(),
+        name="plant-care-topic",
+    ),
+    path(
+        "app/plant-care-topic-get/",
+        CareTopicGetView.as_view(),
+        name="plant-care-topic-get",
+    ),
+    path(
+        "app/plant-care-topic-history/",
+        CareTopicsHistoryView.as_view(),
+        name="plant-care-topic-history",
+    ),
+    path(
+        "app/plant-location-market/",
+        LocationMarketView.as_view(),
+        name="plant-location-market",
+    ),
+    path(
+        "app/plant-care-trees/", CarePlantingTreeView.as_view(), name="plant-care-trees"
+    ),
+    path(
+        "app/plant-recently-viewed/",
+        LastRecentlyViewedPlantView.as_view(),
+        name="plant-recently-viewed",
+    ),
+    path(
+        "app/plant-recently-uploaded/",
+        RecentlyUploadedView.as_view(),
+        name="plant-recently-uploaded",
+    ),
+    path("app/plant-search/", PlantSearchView.as_view(), name="plant-search"),
+    path(
+        "app/plant-care_search/",
+        CarePlantingSearchView.as_view(),
+        name="plant-care_search",
+    ),
+    path(
+        "app/plant-care_tree_search/",
+        CarePlantingTreeSearchView.as_view(),
+        name="plant-care_tree_search",
+    ),
 ]
